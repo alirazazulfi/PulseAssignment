824efa6fe57089d1f9cf1bc055c0add086a4d1c7
Sahibzada Zulfi <zulfi@truemedit.com>
2022-03-28 23:06:17 +0500

    Initial

---
+++ PulseAssignment/BLL/FileRepository.cs
@@ -0,0 +1,30 @@
+﻿using PulseAssignment.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PulseAssignment.BLL
+{
+    public class FileRepository
+    {
+        PulseAssignmentEntities db = new PulseAssignmentEntities();
+
+        public List<ImageFile> GetAllImages()
+        {
+            return db.ImageFiles.ToList();
+        }
+
+        public ImageFile GetImagesById(int? id)
+        {
+            return db.ImageFiles.SingleOrDefault(x => x.Id == id);
+        }
+
+        public bool SaveImage(ImageFile imageFile)
+        {
+            db.ImageFiles.Add(imageFile);
+            db.SaveChanges();
+
+            return true;
+        }
+
+    }
+}
